00ee35d48f5ca5554da20be77c5e1a437fda7ac4
gggggll <1561062061@qq.com>
2017-10-16 22:28:24 +0800

    login code

---
+++ app/src/main/java/com/example/administrator/demo/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by lenovo on 2017/10/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
