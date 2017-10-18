bb62d2f737d1c54e37f63e21acb9bb62d3fdb0ee
gggggll <1561062061@qq.com>
2017-10-18 19:41:34 +0800

    login code

--- app/src/main/java/com/example/administrator/demo/LoginActivity.java
+++ app/src/main/java/com/example/administrator/demo/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 /**
@@ -12,5 +13,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+    public void click(View view){
+
     }
 }
