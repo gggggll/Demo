37905ce282b66e303042c9236c387a07ada8b0ca
gggggll <1561062061@qq.com>
2017-10-19 11:46:09 +0800

    login code

--- app/src/main/java/com/example/administrator/demo/LoginActivity.java
+++ app/src/main/java/com/example/administrator/demo/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.administrator.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -10,11 +12,17 @@ import android.view.View;
  */
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        btn=(Button)findViewById(R.id.btn_dl);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+            Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+            }
+        });
     }
-    public void click(View view){
 
-    }
 }
